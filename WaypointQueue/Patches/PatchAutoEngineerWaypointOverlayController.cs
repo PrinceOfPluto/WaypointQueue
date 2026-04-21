9ee051a0c9d50fefe78c805a6f74433fd11c871f
PrinceOfPluto <theprinceofpluto@gmail.com>
2026-04-21 05:06:18 -0400

    Patch request route to handle null selected locomotive

---
+++ WaypointQueue/Patches/PatchAutoEngineerWaypointOverlayController.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UI.EngineControls;
+
+namespace WaypointQueue.Patches
+{
+    [HarmonyPatch(typeof(AutoEngineerWaypointOverlayController))]
+    internal class PatchAutoEngineerWaypointOverlayController
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("RequestRoute")]
+        static bool RequestRoutePrefix(AutoEngineerWaypointOverlayController __instance)
+        {
+            if (TrainController.Shared.SelectedLocomotive == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
