25f0541e9d1564693becf4c209847e246a9badee
PrinceOfPluto <theprinceofpluto@gmail.com>
2026-05-03 23:02:08 -0400

    Suppress arrived at waypoint notice if queue is not empty

---
+++ WaypointQueue/Patches/PatchNoticeManager.cs
@@ -0,0 +1,28 @@
+﻿using Game.Notices;
+using HarmonyLib;
+using WaypointQueue.State;
+
+namespace WaypointQueue.Patches
+{
+    [HarmonyPatch(typeof(NoticeManager))]
+    internal class PatchNoticeManager
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(NoticeManager.PostEphemeralLocal))]
+        static bool PostEphemeralLocalPrefix(NoticeManager __instance, EntityReference entity, string contextualKey, ref string content)
+        {
+            var atWaypointKey = "ai-wpt";
+
+            if (entity.Type == EntityType.Car && contextualKey == atWaypointKey)
+            {
+                string locoId = entity.Id;
+
+                if (ModStateManager.Shared.LocoWaypointStates.TryGetValue(locoId, out var state) && state.Waypoints.Count > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
